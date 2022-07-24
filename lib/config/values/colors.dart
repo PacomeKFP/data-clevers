23d0e1ff678b22f3fd31fcc5427fdb5a1e9809ff
Nameless0l <wwwmbassiloic@gmail.com>
2022-07-24 18:11:24 +0100

    khik

--- lib/config/values/colors.dart
+++ lib/config/values/colors.dart
@@ -2,12 +2,13 @@ part of settings;
 
 class AppColors {
   // Clors(super.value);
-  static Color col1 = const Color.fromARGB(126, 4, 255, 80);
-  static Color col2 = Color.fromARGB(124, 33, 99, 53);
-  static Color principalColor = Color.fromARGB(255, 0, 69, 98);
-  static Color col3 = Color.fromARGB(255, 0, 69, 98);
+  static Color col2 = const Color.fromARGB(124, 33, 99, 53);
+  static Color principalColor = const Color.fromARGB(255, 0, 69, 98);
+  static Color col4 = const Color(0xFF5FB3D4);
+  static Color col5 = const Color(0xFFD9D9D9);
+  static Color noir = const Color(0xFF000000);
+  static Color blanc = const Color(0xFFFFFEFE);
   // static Color
 
-  
   AppColors._();
 }
